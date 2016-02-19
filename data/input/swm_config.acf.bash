e3a7ce3d715ebc20be52ed58dc1d68a1e30384de
Joe Hamman <jhamman@hydro.washington.edu>
2016-02-19 09:50:38 -0800

    final path mods

--- data/input/swm_config.acf.bash
+++ data/input/swm_config.acf.bash
@@ -11,9 +11,8 @@ swm_stations=/raid2/jhamman/projects/ACF/data/input/acf.stns.info
 regrid_exe=/raid2/jhamman/projects/ACF/scripts/regrid/regrid
 
 # input data directories
-swm_ref_dir=/raid/blivneh/forcings/outputs/NLDAS/asc/
-swm_stn_dir=/raid8/forecast/sw_monitor/data/conus/forcing/acis/lt_data/
-swm_stn_dir_mu=/raid3/muxiao/projects/ACF/data/stns/
+swm_ref_dir=/raid2/jhamman/projects/ACF/data/input/livneh_asc
+swm_stn_dir_mu=/raid2/jhamman/projects/ACF/data/input/stns/
 # output directories
 swm_output_dir=/raid2/jhamman/projects/ACF/data/output/
 swm_force_dir=/raid2/jhamman/projects/ACF/data/forcings/acf_swm/
