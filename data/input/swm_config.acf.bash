cecc6049f94b03421cd07df37e101de6247e93d3
Joe Hamman <jhamman@hydro.washington.edu>
2016-02-20 12:35:21 -0800

    remove large files and update paths

--- data/input/swm_config.acf.bash
+++ data/input/swm_config.acf.bash
@@ -12,7 +12,7 @@ regrid_exe=/raid2/jhamman/projects/ACF/scripts/regrid/regrid
 
 # input data directories
 swm_ref_dir=/raid2/jhamman/projects/ACF/data/input/livneh_asc
-swm_stn_dir_mu=/raid2/jhamman/projects/ACF/data/input/stns/
+swm_stn_dir=/raid2/jhamman/projects/ACF/data/input/stns/
 # output directories
 swm_output_dir=/raid2/jhamman/projects/ACF/data/output/
 swm_force_dir=/raid2/jhamman/projects/ACF/data/forcings/acf_swm/
